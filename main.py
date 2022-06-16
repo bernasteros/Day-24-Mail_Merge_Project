f18fa48293af4c8a1ab532615f143a88f9745994
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2022-06-16 14:19:05 +0200

    Read file and test input with print(file.readlines())

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-# Create a letter using starting_letter.txt for each name in invited_names.txt
 
-#TODO: Replace the [name] placeholder with the actual name.
-#TODO: Save the letters in the folder "ReadyToSend".
 
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+with open("./Input/Letters/starting_letter.txt",mode="r") as file:
+    print(file.readlines())
+
+# TODO: Replace the [name] placeholder with "Name.txt" and put it into the ./Output/ReadyToSend
+
+# TODO: Create a letter using starting_letter.txt for each name in invited_names.txt
+
+# TODO: Save the letters in the folder "ReadyToSend".
