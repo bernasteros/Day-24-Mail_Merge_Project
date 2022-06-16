1d24a4e244f5a23ea8e1329ecb2aebba2b8993a1
Bernhard Stern <91109815+bernasteros@users.noreply.github.com>
2022-06-16 14:31:02 +0200

    Read file and test input with print(file.readlines())

--- main.py
+++ main.py
@@ -5,7 +5,8 @@
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 with open("./Input/Letters/starting_letter.txt",mode="r") as file:
-    print(file.readlines())
+    text = file.readlines()
+    print(text)
 
 # TODO: Replace the [name] placeholder with "Name.txt" and put it into the ./Output/ReadyToSend
 
